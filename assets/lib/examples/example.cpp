49f6c51e98946aa31e7cdce579784fc346aa5d56
anton <snezsky@gmail.com>
2026-03-20 00:49:40 +0300

    fix syntax error in lib/example

--- assets/lib/examples/example.cpp
+++ assets/lib/examples/example.cpp
@@ -6,7 +6,7 @@ int main()
     try
     {
         std::cout << myproject::trim(" The outer spaces will be removed! ") << '\n';
-        std::cout << myproject::split("Splitting also works, or is it?", ',') << '\n';
+        std::cout << myproject::split("Splitting also works, or is it?", ',')[0] << '\n';
         std::cout << myproject::join({"This", "will", "be", "one", "string"}, " ") << '\n';
     }
     catch ([[maybe_unused]] std::exception& e)
