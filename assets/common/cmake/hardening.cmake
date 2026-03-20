61a4ddeddbc8b97470602b23998587bc24701343
anton <snezsky@gmail.com>
2026-03-20 14:36:40 +0300

    normalize gnu|clang regexp

--- assets/common/cmake/hardening.cmake
+++ assets/common/cmake/hardening.cmake
@@ -7,7 +7,7 @@ function(__myproject_target_enable_hardening TARGET_NAME)
         target_compile_options(${TARGET_NAME} PRIVATE /sdl /DYNAMICBASE /guard:cf)
     endif()
 
-    if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang|GNU")
+    if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
         check_cxx_compiler_flag(-fcf-protection CF_PROTECTION)
         check_cxx_compiler_flag(-fstack-protector-strong STACK_PROTECTOR)
         check_cxx_compiler_flag(-fstack-clash-protection CLASH_PROTECTION)
