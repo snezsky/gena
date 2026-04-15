263174f33bae973819448706319881413ff82a85
anton <snezsky@gmail.com>
2026-04-16 00:57:13 +0300

    fix typo in a namespace

--- assets/type/library/examples/example.cpp
+++ assets/type/library/examples/example.cpp
@@ -4,5 +4,5 @@
 // NOLINTNEXTLINE(bugprone-exception-escape)
 int main()
 {
-    std::cout << myrpoject::split("Good luck navigating the vast and winding road ahead", 'n')[0] << '\n';
+    std::cout << myproject::split("Good luck navigating the vast and winding road ahead", 'n')[0] << '\n';
 }
