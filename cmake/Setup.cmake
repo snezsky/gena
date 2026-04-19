cb9add0f98d6402115517424a40764439ea5ac39
anton <snezsky@gmail.com>
2026-04-19 20:58:26 +0300

    remove everything related to windeplpoyqt

--- cmake/Setup.cmake
+++ cmake/Setup.cmake
@@ -30,15 +30,3 @@ function(gena_setup_target TARGET_NAME)
 
 endfunction()
 
-function(gena_run_windeployqt TARGET_NAME)
-
-    if (NOT MSVC)
-        return()
-    endif()
-
-    add_custom_command(
-        TARGET ${TARGET_NAME} POST_BUILD
-        COMMAND ${WINDEPLOYQT_EXECUTABLE} $<TARGET_FILE:${TARGET_NAME}> --no-translations
-        COMMENT "Deploying Qt runtime dependencies for ${TARGET_NAME}")
-
-endfunction()
