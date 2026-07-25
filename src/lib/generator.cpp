34cd0e77a592b29e7a5494963c93d0fb2990d20e
anton <snezsky@gmail.com>
2026-07-25 23:52:27 +0300

    do not copy empty deps folder in case there is only qtest

--- src/lib/generator.cpp
+++ src/lib/generator.cpp
@@ -75,7 +75,8 @@ namespace gena
 
     void Generator::copy_dependencies(const path &source, const path &destination, Dependencies dependencies)
     {
-        if (!dependencies) { return; }
+        /* The qtest dependency does not require any files to be copied */
+        if (!dependencies || dependencies == Dependency::qtest) { return; }
 
         auto add_dependency = [&source, &destination](std::string_view name) {
             fs::copy(source / name, destination / name, fs::copy_options::recursive);
